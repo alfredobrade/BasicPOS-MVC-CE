30e530326a696e5e3d4605f588b74ce90e503efc
Alfredo B <alfredobrade@gmail.com>
2023-07-02 19:21:06 -0300

    init

---
+++ BasicPOS.DAL/Context/BasicPOSContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicPOS.DAL.Context
+{
+    public class BasicPOSContext : DbContext
+    {
+        public BasicPOSContext(DbContextOptions<BasicPOSContext> options) : base(options)
+        {
+            
+        }
+
+
+
+
+    }
+}
---
+++ BasicoPOS_MVC/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace BasicoPOS_MVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
